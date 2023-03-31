71f0cfd3a70e0230e9f67e8738c8525258806494
Georgina <faithwangare750@gmail.com>
2023-03-31 13:16:30 +0300

    completed challenge

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const id = data.id;
+        document.body.append(id);
+      })
+      .catch((error) => {
+        document.body.append(error.message);
+      });
+  }
+  submitData();
